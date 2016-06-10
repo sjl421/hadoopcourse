ce77dec47596cdb17011e8e69eabcca500910b43
xschao <uniquechao@qq.com>
2016-06-11 00:22:17 +0800

    Update LineJobs.java

--- zhangyu2015/src/com/zhangyu/mr/recirus/LineJobs.java
+++ zhangyu2015/src/com/zhangyu/mr/recirus/LineJobs.java
@@ -22,3 +22,40 @@
 		FileInputFormat.addInputPath(job3, new Path(outpath2)) ;
 		FileOutputFormat.setOutputPath(job3, new Path(outpath3));
 		job3.waitForCompletion(true) ;
+
+
+
+
+//******************JobControl******************************************************
+
+Configuration conf = new Configuration() ;
+		//第一部分：作业配置，注意只做配置不做运行
+		//job1的配置
+		ControlledJob joba = new ControlledJob(conf) ;
+		Job job1 = new Job(conf,"jobName1") ;
+		job1.setJarByClass(jobClass1);
+		//...
+		joba.setJob(job1);
+		//job2的配置
+		ControlledJob jobb = new ControlledJob(conf) ;
+		Job job2 = new Job(conf,"jobName2") ;
+		job2.setJarByClass(jobClass2);
+		//...
+		jobb.setJob(job2);
+		//job3的配置
+		ControlledJob jobc = new ControlledJob(conf) ;
+		Job job3 = new Job(conf,"jobName3") ;
+		job3.setJarByClass(jobClass3);
+		//...
+		jobc.setJob(job3);
+		
+		//第二部分：依赖关系配置
+		jobb.addDependingJob(joba) ;
+		jobc.addDependingJob(jobb) ;
+		
+		//第三部分：JobControl配置
+		JobControl jc = new JobControl("abc") ;
+		jc.addJob(joba) ;
+		jc.addJob(jobb) ;
+		jc.addJob(jobc) ;
+//*************************************************************************************
